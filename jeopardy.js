cc9f9b7330d979a8833f3817b64edc02e9f5f92f
Jenny Liu <jennyliuye@gmail.com>
2022-02-09 16:04:13 -0800

    finish basic functions

--- jeopardy.js
+++ jeopardy.js
@@ -24,6 +24,10 @@ let categories = [];
 // getCategoryIds();
 // getCategory(11496);
 
+const $startButton = $("#start-button");
+const $jeopardyTable = $("#jeopardy-table");
+console.log($jeopardyTable);
+
 /** Get NUM_CATEGORIES random categories from API.
  *
  * Returns array of category ids, e.g. [4, 12, 5, 9, 20, 1]
@@ -62,8 +66,8 @@ async function getCategory(catId) {
 
 async function makeCategories() {
   let ids = await getCategoryIds();
-  categories = await Promise.all(ids.map(id => getCategory(id)));
-  console.log(categories)
+  categories = await Promise.all(ids.map((id) => getCategory(id)));
+  console.log(categories);
 }
 
 /** Fill an HTML table with the categories & cells for questions.
@@ -74,11 +78,11 @@ async function makeCategories() {
  *   (initially, just show a "?" where the question/answer would go.)
  * <thead> --> 6, filled with category title
  * <tbody> --> filled with clues within that category
- * first add table, 
+ * first add table,
  *  <thead>
  *    <tr>
  *      <th> loop over category titles and fill with title
- * 
+ *
  *  <tbody>
  *    <tr>
  */
@@ -97,42 +101,40 @@ async function makeCategories() {
 //    }, ...4 more categories ...
 //  ]
 async function fillTable() {
-  await makeCategories();
-  let $table = $("<table>");
-  $("#jeopardy-board").append($table);
-  $table.addClass("jeopardy-table");
+  //   let $table = $("<table>");
+  //   $("#jeopardy-board").append($table);
+  //   $table.addClass("jeopardy-table");
 
   let $thead = $("<thead>");
   let $tr = $("<tr>");
   $tr.addClass("categories");
-  $table.append($thead.append($tr));
-  
+  $jeopardyTable.append($thead.append($tr));
+
   for (let category of categories) {
     $th = $("<th>").text(category.title);
-    $th.addClass('jeopardy-title')
-    $tr.append($th);  
+    $th.addClass("jeopardy-title");
+    $tr.append($th);
   }
-  
+
   let $tbody = $("<tbody>");
 
   for (let row = 0; row < NUM_CLUES_PER_CAT; row++) {
     $tr = $("<tr>");
     $tr.addClass(`clues-${row}`);
-    $table.append($tbody.append($tr));
+    $jeopardyTable.append($tbody.append($tr));
 
     for (let col = 0; col < NUM_CATEGORIES; col++) {
       $td = $("<td>").text("?");
-      $td.addClass('clue');
-      $td.attr('row', row);
-      $td.attr('col', col);
-      $td.attr('showing', 0);
-      $td.on('click', handleClick(evt));
+      $td.addClass("clue");
+      $td.attr("row", row);
+      $td.attr("col", col);
+      $td.attr("showing", null);
+      $td.on("click", handleClick);
       $tr.append($td);
     }
   }
-  
-  //categories[col].clues[row].question)
 
+  //categories[col].clues[row].question)
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -143,17 +145,44 @@ async function fillTable() {
  * - if currently "answer", ignore click
  * */
 
-function handleClick(evt) { }
+function handleClick(evt) {
+  let $target = $(evt.target);
+  let showing = $target.attr("showing");
+  let col = $target.attr("col");
+  let row = $target.attr("row");
+  if (!showing) {
+    $target.text(categories[col].clues[row].question);
+    $target.attr("showing", "question");
+  } else {
+    if (showing === "question") {
+      $target.text(categories[col].clues[row].answer);
+      $target.attr("showing", "answer");
+      $target.addClass("answer");
+    }
+  }
+}
 
 /** Wipe the current Jeopardy board, show the loading spinner,
  * and update the button used to fetch data.
  */
 
-function showLoadingView() { }
+function showLoadingView() {
+  $jeopardyTable.empty();
+  $startButton.text("Loading...");
+  const $loadingSpinner = $("<i>");
+  $startButton.prop("disabled", true);
+  $loadingSpinner.addClass("fas fa-spinner fa-spin");
+  $("#jeopardy-board").append($loadingSpinner);
+}
 
 /** Remove the loading spinner and update the button used to fetch data. */
 
-function hideLoadingView() { }
+function hideLoadingView() {
+  const $loadingSpinner = $(".fa-spinner");
+  $loadingSpinner.remove();
+  $startButton.text("Restart");
+  $startButton.prop("disabled", false);
+}
 
 /** Setup game data and board:
  * - get random category Ids
@@ -164,11 +193,15 @@ function hideLoadingView() { }
 async function setupGameBoard() {
   await makeCategories();
   fillTable();
- }
+}
 
 /** Start game: show loading state, setup game board, stop loading state */
 
-async function setupAndStart() { }
+async function setupAndStart() {
+  showLoadingView();
+  await setupGameBoard();
+  hideLoadingView();
+}
 
 /** At start:
  *
@@ -176,5 +209,5 @@ async function setupAndStart() { }
  * - Add a click handler to your board that will run handleClick
  *   when you click on a clue
  */
-
+$startButton.on("click", setupAndStart);
 // ADD THOSE THINGS HERE
